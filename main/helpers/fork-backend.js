4b4cb4cb6613f01afacd3030f9d1246b3552c88f
Jan Kaßel <jan@kassel.works>
2020-01-15 23:28:53 +0100

    add preliminary backend exit handling

--- main/helpers/fork-backend.js
+++ main/helpers/fork-backend.js
@@ -11,9 +11,16 @@ export default function forkBackend() {
       }
     };
 
-    // FIXME: exit application if backend thread crashes
     workerProcess = fork(__filename, ['--backend']);
     workerProcess.on('message', handleReady);
+    workerProcess.on('exit', code => {
+      if (code !== 0) {
+        throw new Error(
+          `Backend thread unexpectedly exited with code ${code}.`
+        );
+      }
+    });
+
     workerProcess.send({
       event: 'init',
       dataDir: app.getPath('userData'),
