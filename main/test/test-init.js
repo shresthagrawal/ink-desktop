c0648949ee540fc43f50c2ae81af4e6a2894a9a6
Shresth Agrawal <shresthagrawal.31@gmail.com>
2020-01-15 16:33:59 +0530

    Add CD for github releases

--- main/test/test-init.js
+++ main/test/test-init.js
@@ -1,8 +1,9 @@
-import assert from "mocha";
 import chai from "chai";
 import chaiAsPromised from "chai-as-promised";
-import { copySync, remove } from "fs-extra";
+import { readFileSync } from "fs";
 import path from "path";
+import { copySync, remove } from "fs-extra";
+import { initInkFile } from "../lib/ink-file/ink-file";
 
 chai.use(chaiAsPromised);
 chai.should();
@@ -12,18 +13,26 @@ const expect = chai.expect;
 const resDir = "./main/test/res";
 // Tmp directory created as an exact copy of the res directory before test
 const tmpDir = "./main/test/tmp";
-
-const projectA = "resource-test/project/a.als";
+const projectA = "project/a.als";
+const inkJson = {
+  "name": "A",
+  "path": "/Users/shresthagrawal/Desktop/work/GitMusic/ink-electron/main/test/tmp/project",
+  "remoteUrl": "",
+  "tracks": []
+} 
 
 describe('Init', function() {
-    describe ('Create Project', function() {
+    describe ('Init Ink File', function() {
         before(async function() {
             // Create a copy of the sample files.
             // This is important as the parser modifies the origional file.
             copySync(resDir, tmpDir);
         });
         it('test', async function() {
-            (1).should.eql(1);
+            let projectDir = path.parse(path.resolve(tmpDir, projectA)).dir;
+            initInkFile('A', projectDir, '');
+            let inkRaw = readFileSync(path.join(projectDir, 'ink.json'));
+            JSON.parse(inkRaw).should.eql(inkJson);
         });
         after(function() {
             // Cleanup after test
